2becd09173b9e7dcf648dc59224f661637fe2004
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-11-12 08:34:27 +0800

    Create Restaurant.dart

---
+++ lib/Restaurant.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+
+class Restaurant {
+  late String id;
+  late String name;
+  late String description;
+  late String pictureId;
+  late String city;
+  late String rating;
+  late List menus;
+
+  Restaurant({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.pictureId,
+    required this.city,
+    required this.rating,
+    required this.menus,
+  });
+
+  Restaurant.fromJson(Map<String, dynamic> restaurant) {
+    id = restaurant['id'];
+    name = restaurant['name'];
+    description = restaurant['description'];
+    pictureId = restaurant['pictureId'];
+    city = restaurant['city'];
+    rating = restaurant['rating'];
+    menus = restaurant['menus'];
+  }
+
+  List<Restaurant> parseRestaurants(String? json) {
+    if (json == null) {
+      return [];
+    }
+
+    final List parsed = jsonDecode(json);
+    return parsed.map((json) => Restaurant.fromJson(json)).toList();
+  }
+}
