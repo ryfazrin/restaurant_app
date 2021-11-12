d67e274ab306597c30eb0ebfe175a2e91d6022b6
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-11-12 16:55:16 +0800

    Update detail_restaurant_page.dart
    membuat Sliver AppBar di detail page

--- lib/detail_restaurant_page.dart
+++ lib/detail_restaurant_page.dart
@@ -12,84 +12,83 @@ class DetailRestaurantPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(restaurant.name),
-        elevation: 0.0,
-      ),
+      // appBar: AppBar(
+      //   title: Text(restaurant.name),
+      //   elevation: 0.0,
+      // ),
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Stack(children: [
-                Image.network(restaurant.pictureId),
-                // Row(
-                //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                //   children: [
-                //     Card(
-                //       margin: EdgeInsets.all(0),
-                //       child: Row(
-                //         mainAxisSize: MainAxisSize.min,
-                //         children: [
-                //           IconButton(
-                //             onPressed: () {
-                //               Navigator.pop(context);
-                //             },
-                //             icon: Icon(Icons.arrow_back),
-                //           ),
-                //         ],
-                //       ),
-                //       shape: BeveledRectangleBorder(
-                //         borderRadius: BorderRadius.only(
-                //           bottomRight: Radius.circular(16.0),
-                //         ),
-                //       ),
-                //     ),
-                //     Card(
-                //       margin: EdgeInsets.all(0),
-                //       child: Row(
-                //         mainAxisSize: MainAxisSize.min,
-                //         children: [
-                //           Padding(
-                //             padding: EdgeInsets.only(right: 16.0),
-                //             child: Text(
-                //               restaurant.name,
-                //               style: Theme.of(context).textTheme.headline6,
-                //             ),
-                //           ),
-                //           IconButton(
-                //             onPressed: () {
-                //               Navigator.pop(context);
-                //             },
-                //             icon: Icon(Icons.star),
-                //           ),
-                //         ],
-                //       ),
-                //       shape: BeveledRectangleBorder(
-                //         borderRadius: BorderRadius.only(
-                //           bottomRight: Radius.circular(16.0),
-                //         ),
-                //       ),
-                //     ),
-                //   ],
-                // ),
-              ]),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: restaurant.menus.foods.map((food) {
-                  return Text(food.name);
-                }).toList(),
+        child: DetailSliverAppBar(
+          dataRestaurant: restaurant,
+        ),
+        // child: SingleChildScrollView(
+        //   child: Column(
+        //     children: [
+        //       // Stack(children: [
+        //       //   Image.network(restaurant.pictureId),
+        //       // ]),
+        //       Column(
+        //         crossAxisAlignment: CrossAxisAlignment.start,
+        //         children: restaurant.menus.foods.map((food) {
+        //           return Text(food.name);
+        //         }).toList(),
+        //       ),
+        //       SizedBox(height: 16.0),
+        //       Column(
+        //         crossAxisAlignment: CrossAxisAlignment.start,
+        //         children: restaurant.menus.drinks.map((drink) {
+        //           return Text(drink.name);
+        //         }).toList(),
+        //       ),
+        //     ],
+        //   ),
+        // ),
+      ),
+    );
+  }
+}
+
+class DetailSliverAppBar extends StatelessWidget {
+  final Restaurant dataRestaurant;
+
+  const DetailSliverAppBar({Key? key, required this.dataRestaurant})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          pinned: true,
+          expandedHeight: 300.0,
+          elevation: 0.0,
+          flexibleSpace: FlexibleSpaceBar(
+            title: Text(dataRestaurant.name),
+            centerTitle: true,
+            background: Image.network(
+              dataRestaurant.pictureId,
+              fit: BoxFit.fill,
+            ),
+          ),
+          leading: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CircleAvatar(
+              backgroundColor: Colors.white,
+              child: IconButton(
+                icon: Icon(Icons.arrow_back),
+                color: Colors.red,
+                onPressed: () {
+                  Navigator.pop(context);
+                },
               ),
-              SizedBox(height: 16.0),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: restaurant.menus.drinks.map((drink) {
-                  return Text(drink.name);
-                }).toList(),
-              ),
-            ],
+            ),
           ),
         ),
-      ),
+        SliverFillRemaining(
+          child: Center(
+            child: Text('Hello'),
+          ),
+        ),
+      ],
     );
   }
 }
