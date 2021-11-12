f226bb1710a87d8642231cb354b3407eac9e2450
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-11-12 13:29:59 +0800

    Update restaurant_list_page.dart
    GridView.builder

--- lib/restaurant_list_page.dart
+++ lib/restaurant_list_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:restaurant_app/detail_restaurant_page.dart';
 import 'package:restaurant_app/model/Restaurant.dart';
 
 class RestaurantListPage extends StatelessWidget {
@@ -18,7 +17,14 @@ class RestaurantListPage extends StatelessWidget {
             .loadString('assets/local_restaurant.json'),
         builder: (context, snapshot) {
           final List<Restaurant> restaurants = parseRestaurants(snapshot.data);
-          return ListView.builder(
+
+          return GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: MediaQuery.of(context).size.width /
+                  (MediaQuery.of(context).size.height / 1.25),
+            ),
+            shrinkWrap: true,
             itemCount: restaurants.length,
             itemBuilder: (context, index) {
               return _buildRestaurantItem(context, restaurants[index]);
@@ -30,20 +36,63 @@ class RestaurantListPage extends StatelessWidget {
   }
 
   Widget _buildRestaurantItem(BuildContext context, Restaurant restaurant) {
-    return ListTile(
-      isThreeLine: true,
-      leading: Image.network(
-        restaurant.pictureId,
-        width: 100,
+    return InkWell(
+      child: ListView(
+        children: [
+          Stack(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height / 3.6,
+                width: MediaQuery.of(context).size.width / 2.2,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: Image.network(
+                    restaurant.pictureId,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Positioned(
+                right: -10.0,
+                bottom: 3.0,
+                child: RawMaterialButton(
+                  onPressed: () {},
+                  fillColor: Colors.white,
+                  shape: CircleBorder(),
+                  elevation: 4.0,
+                  child: Padding(
+                    padding: EdgeInsets.all(5),
+                    child: Icon(
+                      Icons.favorite_border,
+                      color: Colors.red,
+                      size: 17,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
-      title: Text(restaurant.name),
-      subtitle: Text(restaurant.city),
-      // subtitle: Text(restaurant.menus.foods.map((e) => e.name).toString()),
-      trailing: Text(restaurant.rating.toString()),
-      onTap: () {
-        Navigator.pushNamed(context, DetailRestaurantPage.routeName,
-            arguments: restaurant);
-      },
+      onTap: () {},
     );
   }
 }
+
+// Widget _buildRestaurantItem(BuildContext context, Restaurant restaurant) {
+//   return ListTile(
+//     isThreeLine: true,
+//     leading: Image.network(
+//       restaurant.pictureId,
+//       width: 100,
+//     ),
+//     title: Text(restaurant.name),
+//     subtitle: Text(restaurant.city),
+//     // subtitle: Text(restaurant.menus.foods.map((e) => e.name).toString()),
+//     trailing: Text(restaurant.rating.toString()),
+//     onTap: () {
+//       Navigator.pushNamed(context, DetailRestaurantPage.routeName,
+//           arguments: restaurant);
+//     },
+//   );
+// }
