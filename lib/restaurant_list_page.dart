506dcd6748d1d13b0e6abfe427ff938f9105041e
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-11-12 09:40:29 +0800

    Create restaurant_list_page.dart

---
+++ lib/restaurant_list_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:restaurant_app/model/Restaurant.dart';
+
+class RestaurantListPage extends StatelessWidget {
+  static const routeName = '/restaurant_list';
+
+  const RestaurantListPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Restaurant App'),
+      ),
+      body: FutureBuilder<dynamic>(
+        future: DefaultAssetBundle.of(context)
+            .loadString('assets/local_restaurant.json'),
+        builder: (context, snapshot) {
+          final List<Restaurant> restaurants = parseRestaurants(snapshot.data);
+          return ListView.builder(
+            itemCount: restaurants.length,
+            itemBuilder: (context, index) {
+              return _buildRestaurantItem(context, restaurants[index]);
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildRestaurantItem(BuildContext context, Restaurant restaurant) {
+    return ListTile(
+      isThreeLine: true,
+      leading: Image.network(
+        restaurant.pictureId,
+        width: 100,
+      ),
+      title: Text(restaurant.name),
+      subtitle: Text(restaurant.city),
+      trailing: Text(restaurant.rating.toString()),
+    );
+  }
+}
