392803adc16cef5dacaa88aa5d7bf2277c2caa4c
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-11-12 09:19:09 +0800

    Update main.dart
    Mengambil data dan Membuat list

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:restaurant_app/Restaurant.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,12 +20,34 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Restaurant App'),
         ),
-        body: FutureBuilder<String>(
+        body: FutureBuilder<dynamic>(
           future: DefaultAssetBundle.of(context)
               .loadString('assets/local_restaurant.json'),
-          builder: (context, snapshot) {},
+          builder: (context, snapshot) {
+            final List<Restaurant> restaurants =
+                parseRestaurants(snapshot.data);
+            return ListView.builder(
+              itemCount: restaurants.length,
+              itemBuilder: (context, index) {
+                return _buildRestaurantItem(context, restaurants[index]);
+              },
+            );
+          },
         ),
       ),
+    );
+  }
+
+  Widget _buildRestaurantItem(BuildContext context, Restaurant restaurant) {
+    return ListTile(
+      isThreeLine: true,
+      leading: Image.network(
+        restaurant.pictureId,
+        width: 100,
+      ),
+      title: Text(restaurant.name),
+      subtitle: Text(restaurant.city),
+      trailing: Text(restaurant.rating.toString()),
     );
   }
 }
