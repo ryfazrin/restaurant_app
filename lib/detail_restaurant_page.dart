453ab3bc5c5d3247d371ab9ef4d4e90bf52843a2
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-11-12 16:06:05 +0800

    Update detail_restaurant_page.dart
    Custom Appbar

--- lib/detail_restaurant_page.dart
+++ lib/detail_restaurant_page.dart
@@ -14,18 +14,30 @@ class DetailRestaurantPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(restaurant.name),
+        shape: BeveledRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomRight: Radius.circular(16.0),
+          ),
+        ),
+        elevation: 0.0,
       ),
       body: SingleChildScrollView(
         scrollDirection: Axis.horizontal,
         child: Column(
           children: [
+            Text(
+              'Cocot e!',
+              style: TextStyle(fontSize: 50.0),
+            ),
             Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: restaurant.menus.foods.map((food) {
                 return Text(food.name);
               }).toList(),
             ),
             SizedBox(height: 16.0),
             Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: restaurant.menus.drinks.map((drink) {
                 return Text(drink.name);
               }).toList(),
