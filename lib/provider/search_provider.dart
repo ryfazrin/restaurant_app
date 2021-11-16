19654f8a0e0a449eb4974a16cdfad0da993fd5a8
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-11-16 19:36:20 +0800

    First Search Provider

---
+++ lib/provider/search_provider.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:restaurant_app/api/api_service.dart';
+import 'package:restaurant_app/model/restaurant.dart';
+
+enum ResultState { Loading, NoData, HasData, Error }
+
+class SearchProvider extends ChangeNotifier {
+  final ApiService apiService;
+
+  SearchProvider({required this.apiService}) {
+    fetchRestaurantSearch(query);
+  }
+
+  late RestaurantSearchResult _restaurantSearchResult;
+  late ResultState _state;
+  String _query = '';
+
+  String get query => _query;
+
+  String _message = '';
+
+  String get message => _message;
+
+  RestaurantSearchResult get result => _restaurantSearchResult;
+
+  ResultState get state => _state;
+
+  Future<dynamic> fetchRestaurantSearch(String query) async {
+    try {
+      _state = ResultState.Loading;
+      _query = query;
+
+      final restaurantSearch = await apiService.getSearchRestaurant(query);
+      if (restaurantSearch.restaurants.isEmpty) {
+        _state = ResultState.NoData;
+        notifyListeners();
+        return _message = 'Data Tidak Ditemukan';
+      } else {
+        _state = ResultState.HasData;
+        notifyListeners();
+        return _restaurantSearchResult = restaurantSearch;
+      }
+    } catch (e) {
+      _state = ResultState.Error;
+      notifyListeners();
+      return _message = 'Error --> $e';
+    }
+  }
+}
--- lib/ui/search_page.dart
+++ lib/ui/search_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:restaurant_app/api/api_service.dart';
-import 'package:restaurant_app/model/restaurant.dart';
+import 'package:restaurant_app/provider/search_provider.dart';
 import 'package:restaurant_app/widget/card.dart';
 
 class SearchPage extends StatefulWidget {
@@ -14,87 +15,150 @@ class SearchPage extends StatefulWidget {
 
 class _SearchPageState extends State<SearchPage> {
   final textFieldController = TextEditingController();
-  String search = "";
-  late Future<RestaurantSearchResult> _restaurantSearch =
-      ApiService().getSearchRestaurant(search);
+  String queries = '';
+  // late Future<RestaurantSearchResult> _restaurantSearch =
+  //     ApiService().getSearchRestaurant(search);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Container(
-          width: double.infinity,
-          height: 40,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(5),
-          ),
-          child: Center(
-            child: TextField(
-              controller: textFieldController,
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.search),
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.clear),
-                  onPressed: () {
-                    textFieldController.clear();
+    return ChangeNotifierProvider<SearchProvider>(
+      create: (_) => SearchProvider(apiService: ApiService()),
+      child:
+          Consumer<SearchProvider>(builder: (context, SearchProvider state, _) {
+        Future<dynamic> _restaurantSearch =
+            state.fetchRestaurantSearch(queries);
+        // state.fetchRestaurantSearch(queries);
+
+        return Scaffold(
+          appBar: AppBar(
+            title: Container(
+              width: double.infinity,
+              height: 40,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Center(
+                child: TextField(
+                  controller: textFieldController,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.search),
+                    suffixIcon: IconButton(
+                      icon: Icon(Icons.clear),
+                      onPressed: () {
+                        textFieldController.clear();
+                      },
+                    ),
+                    hintText: 'Search Restaurant',
+                    border: InputBorder.none,
+                  ),
+                  onChanged: (text) {
+                    // setState(() {
+                    //   queries = text;
+                    // });
+                    // _restaurantSearch = state.fetchRestaurantSearch(search);
+                    if (text != '') {
+                      state.fetchRestaurantSearch(text);
+                    }
                   },
                 ),
-                hintText: 'Search Restaurant',
-                border: InputBorder.none,
               ),
-              onChanged: (text) {
-                setState(() {
-                  search = text;
-                  _restaurantSearch = ApiService().getSearchRestaurant(search);
-                });
-              },
             ),
           ),
-        ),
-      ),
-      body: _buildList(),
+          body: _buildListView(state),
+        );
+      }),
     );
   }
 
-  Widget _buildList() {
-    return FutureBuilder(
-      future: _restaurantSearch,
-      builder: (context, AsyncSnapshot<RestaurantSearchResult> snapshot) {
-        var state = snapshot.connectionState;
-
-        if (state != ConnectionState.done) {
-          // loading widget
-          return Center(child: CircularProgressIndicator());
-        } else {
-          if (snapshot.hasData) {
-            // success widget
-            if (snapshot.data?.founded == 0) {
-              return Center(
-                child: Text('Restaurant tidak ditemukan :('),
-              );
-            } else {
-              return ListView.builder(
-                shrinkWrap: true,
-                physics: ScrollPhysics(),
-                itemCount: snapshot.data?.restaurants.length,
-                itemBuilder: (BuildContext context, int index) {
-                  var restaurant = snapshot.data?.restaurants[index];
-                  return CardRestaurantSearch(restaurant: restaurant);
-                },
-              );
-            }
-          } else if (snapshot.hasError) {
-            // error widget
-            return Center(
-              child: Text('Silahkan ketik restaurant yang anda cari'),
-            );
-          } else {
-            // loading widget
-            return Center(child: Text('tidak ditemukan'));
-          }
-        }
+  ListView _buildListView(SearchProvider state) {
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: ScrollPhysics(),
+      itemCount: state.result.restaurants.length,
+      itemBuilder: (BuildContext context, int index) {
+        var restaurant = state.result.restaurants[index];
+        return CardRestaurantSearch(restaurant: restaurant);
       },
     );
   }
+
+  // Widget _buildList() {
+  //   return Consumer<SearchProvider>(
+  //     builder: (context, SearchProvider state, _) {
+  //       if (state.state == ResultState.Loading) {
+  //         // loading widget
+  //         return Center(child: CircularProgressIndicator());
+  //       } else if (state.state == ResultState.HasData) {
+  //         return ListView.builder(
+  //           shrinkWrap: true,
+  //           physics: ScrollPhysics(),
+  //           itemCount: state.result.restaurants.length,
+  //           itemBuilder: (BuildContext context, int index) {
+  //             var restaurant = state.result.restaurants[index];
+  //             return CardRestaurantSearch(restaurant: restaurant);
+  //           },
+  //         );
+  //       } else if (state.state == ResultState.NoData) {
+  //         return Center(child: Text(state.message));
+  //       } else if (state.state == ResultState.Error) {
+  //         return Center(child: Text(state.message));
+  //       } else {
+  //         return Center(child: Text(''));
+  //       }
+  // =========lama
+  // if (snapshot.hasData) {
+  //   // success widget
+  // } else if (snapshot.hasError) {
+  //   // error widget
+  //   return Center(
+  //     child: Text('Silahkan ketik restaurant yang anda cari'),
+  //   );
+  // } else {
+  //   // loading widget
+  //   return Center(child: Text('tidak ditemukan'));
+  // }
+  //   },
+  // );
+
+  // }Widget _buildList() {
+  //   return FutureBuilder(
+  //     future: _restaurantSearch,
+  //     builder: (context, AsyncSnapshot<RestaurantSearchResult> snapshot) {
+  //       var state = snapshot.connectionState;
+  //
+  //       if (state != ConnectionState.done) {
+  //         // loading widget
+  //         return Center(child: CircularProgressIndicator());
+  //       } else {
+  //         if (snapshot.hasData) {
+  //           // success widget
+  //           if (snapshot.data?.founded == 0) {
+  //             return Center(
+  //               child: Text('Restaurant tidak ditemukan :('),
+  //             );
+  //           } else {
+  //             return ListView.builder(
+  //               shrinkWrap: true,
+  //               physics: ScrollPhysics(),
+  //               itemCount: snapshot.data?.restaurants.length,
+  //               itemBuilder: (BuildContext context, int index) {
+  //                 var restaurant = snapshot.data?.restaurants[index];
+  //                 return CardRestaurantSearch(restaurant: restaurant);
+  //               },
+  //             );
+  //           }
+  //         } else if (snapshot.hasError) {
+  //           // error widget
+  //           return Center(
+  //             child: Text('Silahkan ketik restaurant yang anda cari'),
+  //           );
+  //         } else {
+  //           // loading widget
+  //           return Center(child: Text('tidak ditemukan'));
+  //         }
+  //       }
+  //     },
+  //   );
+  // }
 }
