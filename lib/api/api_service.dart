3b0e832d6469f1eab8a54ef1cac8a7da4f802d28
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-11-16 12:07:52 +0800

    Update api_service.dart
    Tambahkan api service detail restaurant

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -2,14 +2,24 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:restaurant_app/model/restaurant.dart';
+import 'package:restaurant_app/model/restaurant_detail.dart';
 
 class ApiService {
   static final String _baseUrl = 'https://restaurant-api.dicoding.dev/';
 
   Future<RestaurantResult> topHeadlines() async {
-    final response = await http.get(Uri.parse('${_baseUrl}list'));
+    final response = await http.get(Uri.parse(_baseUrl + 'list'));
     if (response.statusCode == 200) {
       return RestaurantResult.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load top headlines');
+    }
+  }
+
+  Future<RestaurantDetailResult> foundDetail(String id) async {
+    final response = await http.get(Uri.parse(_baseUrl + 'detail/' + id));
+    if (response.statusCode == 200) {
+      return RestaurantDetailResult.fromJson(json.decode(response.body));
     } else {
       throw Exception('Failed to load top headlines');
     }
