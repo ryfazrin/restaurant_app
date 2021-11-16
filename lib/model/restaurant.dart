55212f977e2745b2c2e02dcb29717af74c4b85a9
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-11-16 16:10:01 +0800

    Update restaurant.dart
    RestaurantSearchResult

--- lib/model/restaurant.dart
+++ lib/model/restaurant.dart
@@ -34,6 +34,32 @@ class RestaurantResult {
       };
 }
 
+class RestaurantSearchResult {
+  RestaurantSearchResult({
+    required this.error,
+    required this.founded,
+    required this.restaurants,
+  });
+
+  bool error;
+  int founded;
+  List<Restaurant> restaurants;
+
+  factory RestaurantSearchResult.fromJson(Map<String, dynamic> json) =>
+      RestaurantSearchResult(
+        error: json["error"],
+        founded: json["founded"],
+        restaurants: List<Restaurant>.from(
+            json["restaurants"].map((x) => Restaurant.fromJson(x))),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "error": error,
+        "founded": founded,
+        "restaurants": List<dynamic>.from(restaurants.map((x) => x.toJson())),
+      };
+}
+
 class Restaurant {
   Restaurant({
     required this.id,
