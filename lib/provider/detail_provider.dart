2832895e87ac78ff8b96e07c424e788640f6c77f
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-11-17 19:26:04 +0800

    Create detail_provider.dart

---
+++ lib/provider/detail_provider.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/cupertino.dart';
+import 'package:restaurant_app/api/api_service.dart';
+import 'package:restaurant_app/model/restaurant_detail.dart';
+
+enum ResultState { Loading, NoData, HasData, Error }
+
+class DetailProvider extends ChangeNotifier {
+  late final ApiService apiService;
+  late final String id;
+
+  DetailProvider({required this.apiService, required this.id}) {
+    _fetchRestaurantDetail(id);
+  }
+
+  late RestaurantDetailResult _restaurantDetailResult;
+  late ResultState _state;
+  String _message = '';
+
+  String get message => _message;
+
+  RestaurantDetailResult get result => _restaurantDetailResult;
+
+  ResultState get state => _state;
+
+  Future<dynamic> _fetchRestaurantDetail(String id) async {
+    try {
+      _state = ResultState.Loading;
+      notifyListeners();
+      final restaurantDetail = await apiService.foundDetail(id);
+      if (restaurantDetail.error == true) {
+        _state = ResultState.NoData;
+        notifyListeners();
+        return _message = 'Empty Data';
+      } else {
+        _state = ResultState.HasData;
+        notifyListeners();
+        return _restaurantDetailResult = restaurantDetail;
+      }
+    } catch (e) {
+      _state = ResultState.Error;
+      notifyListeners();
+      return _message = 'Sepertinya ada Masalah dengan jaringan anda :(';
+    }
+  }
+}
