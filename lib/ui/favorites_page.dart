ac795ff82f65c352c0a3acb27d1d1ca65bb574b1
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-11-18 22:59:57 +0800

    Update favorites_page.dart
    DatabaseProvider dan ListView

--- lib/ui/favorites_page.dart
+++ lib/ui/favorites_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:restaurant_app/db/database_helper.dart';
+import 'package:restaurant_app/provider/database_provider.dart';
+import 'package:restaurant_app/widget/card.dart';
 
 class FavoritesPage extends StatelessWidget {
   static const routeName = '/favorites_restaurant';
@@ -11,8 +15,19 @@ class FavoritesPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Favorites'),
       ),
-      body: Container(
-        child: Text('Body'),
+      body: ChangeNotifierProvider(
+        create: (_) => DatabaseProvider(databaseHelper: DatabaseHelper()),
+        child: Consumer<DatabaseProvider>(builder: (context, provider, child) {
+          return ListView.builder(
+            shrinkWrap: true,
+            physics: ScrollPhysics(),
+            itemCount: provider.favorites.length,
+            itemBuilder: (BuildContext context, int index) {
+              var restaurant = provider.favorites[index];
+              return CardRestaurantSearch(restaurant: restaurant);
+            },
+          );
+        }),
       ),
     );
   }
