8d4e3f0243666432cb1f1f88e97f5e7a77669446
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-11-18 16:29:04 +0800

    Create database_provider.dart

---
+++ lib/provider/database_provider.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:restaurant_app/db/database_helper.dart';
+import 'package:restaurant_app/model/restaurant.dart';
+
+enum ResultState { Loading, NoData, HasData, Error }
+
+class DatabaseProvider extends ChangeNotifier {
+  final DatabaseHelper databaseHelper;
+
+  DatabaseProvider({required this.databaseHelper}) {
+    _getFavorites();
+  }
+
+  late ResultState _state;
+  ResultState get state => _state;
+
+  String _message = '';
+  String get message => _message;
+
+  List<Restaurant> _favorites = [];
+  List<Restaurant> get favorites => _favorites;
+
+  void _getFavorites() async {
+    _favorites = await databaseHelper.getFavorites();
+    if (_favorites.isNotEmpty) {
+      _state = ResultState.HasData;
+    } else {
+      _state = ResultState.NoData;
+      _message = 'Empty Data';
+    }
+    notifyListeners();
+  }
+}
