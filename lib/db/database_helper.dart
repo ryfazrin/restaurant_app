0cf4a7eb48704ff005999fad87d60f92edb60d8f
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-11-18 16:17:57 +0800

    Create database_helper.dart

---
+++ lib/db/database_helper.dart
@@ -0,0 +1,42 @@
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static DatabaseHelper? _instance;
+  static Database? _database;
+
+  DatabaseHelper._internal() {
+    _instance = this;
+  }
+
+  factory DatabaseHelper() => _instance ?? DatabaseHelper._internal();
+
+  static const String _tblFavorite = 'favorites';
+
+  Future<Database> _initializeDb() async {
+    var path = await getDatabasesPath();
+    var db = openDatabase(
+      '$path/newsapp.db',
+      onCreate: (db, version) async {
+        await db.execute('''CREATE TABLE $_tblFavorite (
+          id TEXT PRIMARY KEY,
+          name TEXT,
+          title TEXT,
+          description TEXT,
+          pictureId TEXT,
+          city TEXT,
+          rating INTEGER
+        )
+        ''');
+      },
+      version: 1,
+    );
+
+    return db;
+  }
+
+  Future<Database?> get database async {
+    _database ??= await _initializeDb();
+
+    return _database;
+  }
+}
