30094c6b9a7dc15663e5fac17812ad8deed94b3b
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-11-19 09:51:56 +0800

    Update settings_page.dart
    MultiProvider, PreferencesProvider, dan SchedulingProvider

--- lib/ui/settings_page.dart
+++ lib/ui/settings_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:restaurant_app/preferences/preferences_helper.dart';
+import 'package:restaurant_app/provider/preferences_provider.dart';
 import 'package:restaurant_app/provider/scheduling_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsPage extends StatelessWidget {
   static const routeName = '/settings';
@@ -9,26 +12,40 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<SchedulingProvider>(
-      create: (_) => SchedulingProvider(),
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Settings'),
-        ),
-        body: ListTile(
-          title: Text('Scheduling Restaurant'),
-          trailing: Consumer<SchedulingProvider>(
-            builder: (context, scheduled, _) {
-              return Switch.adaptive(
-                value: false,
-                onChanged: (value) async {
-                  scheduled.scheduledRestaurant(value);
-                },
-              );
-            },
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<PreferencesProvider>(
+          create: (_) => PreferencesProvider(
+            preferencesHelper: PreferencesHelper(
+              sharedPreferences: SharedPreferences.getInstance(),
+            ),
           ),
         ),
-      ),
+        ChangeNotifierProvider<SchedulingProvider>(
+          create: (_) => SchedulingProvider(),
+        ),
+      ],
+      child: Consumer<PreferencesProvider>(builder: (context, provider, child) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('Settings'),
+          ),
+          body: ListTile(
+            title: Text('Scheduling Restaurant'),
+            trailing: Consumer<SchedulingProvider>(
+              builder: (context, scheduled, _) {
+                return Switch.adaptive(
+                  value: provider.isDailyRestaurantsActive,
+                  onChanged: (value) async {
+                    scheduled.scheduledRestaurant(value);
+                    provider.enableDailyRestaurants(value);
+                  },
+                );
+              },
+            ),
+          ),
+        );
+      }),
     );
   }
 }
